7c81daba42da288bc75e5b9fe3392d3465903f2a
CREAsTIVE <62616308+CREAsTIVE@users.noreply.github.com>
2025-12-21 03:54:30 +0500

    Добавьте файлы проекта.

---
+++ Points/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Points
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
