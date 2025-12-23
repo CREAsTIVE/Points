1bee7a4ac0ad6ee684402c68ec94214ae3837728
CREAsTIVE <62616308+CREAsTIVE@users.noreply.github.com>
2025-12-24 04:49:41 +0500

    feat: signal selection window

---
+++ Points/Windows/SignalSelection/SignalSelectionViewModel.cs
@@ -0,0 +1,51 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Points.Models;
+using Points.Services.Database;
+using Points.Windows.SignalCreation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+
+namespace Points.Windows.SignalSelection; 
+public partial class SignalSelectionViewModel : ObservableObject {
+	IDbContextFactory<SignalDbContext> dbFactory;
+	Func<SignalCreationWindow> signalCreationWindowFactory;
+	public SignalSelectionViewModel(IDbContextFactory<SignalDbContext> dbFactory, Func<SignalCreationWindow> signalCreationWindowFactory) {
+		this.dbFactory = dbFactory;
+		this.signalCreationWindowFactory = signalCreationWindowFactory;
+		_ = Load();
+	}
+
+	[ObservableProperty]
+	ObservableCollection<SignalModel> signals = new();
+
+	[ObservableProperty]
+	SignalModel? selectedSignal = null;
+
+	public async Task Load() {
+		using (var db = dbFactory.CreateDbContext()) {
+			Signals = new((await db.SignalsMeta.Select(meta => meta).ToListAsync()).Select(entity => new SignalModel(entity)));
+		}
+	}
+
+	public SignalSelectionWindow? parentWindow;
+
+	[RelayCommand]
+	void OpenCreateSignalWindow() {
+		var window = signalCreationWindowFactory();
+		window.OnCreated += () => parentWindow?.Close();
+		window.ShowDialog();
+	}
+
+	[RelayCommand]
+	void SelectSignal(SignalModel signal) {
+
+	}
+}
---
+++ Points/Windows/SignalSelection/SignalSelectionWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Points.Services.Database;
+using Points.Windows.SignalCreation;
+using Points.Windows.SignalSelection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Points.Windows {
+	/// <summary>
+	/// Interaction logic for SignalSelectionWindow.xaml
+	/// </summary>
+	public partial class SignalSelectionWindow : Window {
+		public SignalSelectionWindow(IServiceProvider serviceProvider) {
+			InitializeComponent();
+
+			var dataContext = serviceProvider.GetRequiredService<SignalSelectionViewModel>(); ;
+			dataContext.window = this;
+			DataContext = dataContext;
+		}
+    }
+}
